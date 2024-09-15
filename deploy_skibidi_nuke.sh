d5453b8e291c47bdcfd5bf921864025dcfd4a095
r0ddty <r0d@ukr.net>
2024-09-15 15:08:23 +0300

    change device tree branch
    Signed-off-by: r0ddty <r0d@ukr.net>

--- deploy_skibidi_nuke.sh
+++ deploy_skibidi_nuke.sh
@@ -6,8 +6,7 @@ echo "syncing source"; \
 /opt/crave/resync.sh; \
 echo "synced source"; \
 echo "cloning device sources"; \
-git clone https://github.com/r0ddty/device_xiaomi_fog --depth 1 -b rising-qpr3 device/xiaomi/fog; \
-git clone https://gitlab.com/r0ddty/vendor_xiaomi_miuicamera_fog --depth 1 -b leica-5.0 vendor/xiaomi/miuicamera; \
+git clone https://github.com/r0ddty/device_xiaomi_fog --depth 1 -b rising-staging device/xiaomi/fog; \
 echo "device source cloned"; \
 echo "starting build"; \
 source build/envsetup.sh; \
